be9aacb84f20643217824529c8cfb2561669302b
Achim Ledermüller <achim.ledermueller@netways.de>
2014-06-18 10:21:17 +0200

    [opennebula] added mock for template_pool

--- lib/fog/opennebula/requests/compute/template_pool.rb
+++ lib/fog/opennebula/requests/compute/template_pool.rb
@@ -53,6 +53,7 @@ module Fog
 
             # h["NIC"] has to be an array of nic objects
             nics = h["NIC"] unless h["NIC"].nil?
+            #Fog::Logger.info("template_pool: Found NIC: #{nics.inspect}")
             h["NIC"] = [] # reset nics to a array
             if nics.is_a? Array
               nics.each do |n|
@@ -61,17 +62,24 @@ module Fog
                 elsif n["NETWORK"]
                   vnet = networks.get_by_name(n["NETWORK"].to_s)
                 else 
+                  #Fog::Logger.info("template_pool: Could not identify the NETWORK within the flavor #{h[:name]}")
                   next
                 end
                 h["NIC"] << interfaces.new({ :vnet => vnet, :model => n["MODEL"] || "virtio" })
+                #Fog::Logger.info("template_pool: Added nic #{h['NIC'].last} to the flavor #{h[:name]}")
               end
             elsif nics.is_a? Hash
+              #Fog::Logger.info("template_pool: nic is a hash: #{nics.inspect}")
               if nics["NETWORK_ID"]
                 vnet = networks.get(nics["NETWORK_ID"].to_s)
-              elsif n["NETWORK"]
+              elsif nics["NETWORK"]
                 vnet = networks.get_by_name(nics["NETWORK"].to_s)
+              else
+                #Fog::Logger.info("template_pool: Could not identify the NETWORK within the flavor #{h[:name]}")
+                next
               end
               h["NIC"] << interfaces.new({ :vnet => vnet, :model => nics["model"]})
+              #Fog::Logger.info("template_pool: Added nic #{h['NIC'].last} to the flavor #{h[:name]}")
             else
               # should i break?
             end
@@ -81,18 +89,48 @@ module Fog
             h.each_pair do |k,v| 
               ret_hash.merge!({k.downcase => v}) 
             end
+            #Fog::Logger.info("ret_h: #{ret_hash}")
             ret_hash
           end
           
           templates.delete nil
           raise Fog::Compute::OpenNebula::NotFound, "Flavor/Template not found" if templates.empty?
+          #Fog::Logger.info("template_pool: #{templates.inspect}")
           templates
         end #def template_pool
       end #class Real
 
       class Mock
         def template_pool(filter = { })
-          [ {}, {} ]
+
+          nic1 = Mock_nic.new
+          nic1.vnet = networks.first
+          [ 
+            {
+              :id       => "4", 
+              :name     => "fogtest", 
+              :content  => "CPU=\"0.1\"\nDISK=[\n  IMAGE=\"tt\",\n  IMAGE_UNAME=\"oneadmin\" ]\nDISK=[\n  IMAGE=\"testimage\",\n  IMAGE_UNAME=\"oneadmin\" ]\nGRAPHICS=[\n  LISTEN=\"0.0.0.0\",\n  TYPE=\"VNC\" ]\nMEMORY=\"126\"\nNIC=[\n  NETWORK=\"fogtest\",\n  NETWORK_UNAME=\"oneadmin\" ]\nOS=[\n  ARCH=\"x86_64\" ]\nSCHED_REQUIREMENTS=\"NFS=true\"\nVCPU=\"2\"", "cpu"=>"0.1",
+              "disk" => [{"IMAGE"=>"tt", "IMAGE_UNAME"=>"oneadmin"}, {"IMAGE"=>"testimage", "IMAGE_UNAME"=>"oneadmin"}],
+              "graphics"=>{"LISTEN"=>"0.0.0.0", "TYPE"=>"VNC"},
+              "memory"=>"126",
+              "sched_requirements"=>"NFS=true",
+              "vcpu"=>"2",
+              "nic" => [ nic1 ] ,
+              "os" => "crux"
+            }
+          ]
+        end
+
+        class Mock_nic 
+          attr_accessor :vnet
+
+          def id
+            2
+          end
+          def name
+            "fogtest"
+          end
+
         end
       end #class Mock
     end #class OpenNebula
